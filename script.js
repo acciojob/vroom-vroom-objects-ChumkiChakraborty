6f56d22d72950af996e23af6ffa1225764be5e51
ChumkiChakraborty <137317244+ChumkiChakraborty@users.noreply.github.com>
2024-09-24 22:07:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,31 @@
-// Complete the js code
-function Car(make, model) {}
+// Complete this js code
+function Person(name, age) {
+  this.name = name;
+  this.age = age;
+}
 
-function SportsCar(make, model, topSpeed) {}
+Person.prototype.greet = function() {
+  // Adjusted to match the expected output format
+  console.log(`Hello, my name is ${this.name} and I am ${this.age} years old`);
+};
 
-// Do not change the code below
-window.Car = Car;
-window.SportsCar = SportsCar;
+function Employee(name, age, jobTitle) {
+  // Call the Person constructor to initialize name and age
+  Person.call(this, name, age);
+  this.jobTitle = jobTitle;
+}
+
+// Inherit the Person prototype for Employee
+Employee.prototype = Object.create(Person.prototype);
+
+// Correct the constructor reference for Employee
+Employee.prototype.constructor = Employee;
+
+Employee.prototype.jobGreet = function() {
+  // Adjusted to match the expected output format
+  console.log(`Hello, my name is ${this.name}, I am ${this.age} years old, and my job title is ${this.jobTitle}`);
+};
+
+// Do not change code below this line
+window.Person = Person;
+window.Employee = Employee;
